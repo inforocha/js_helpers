b1faa192d99ae4741316381827a2c0d94fee3dd7
Davi Samuel <davisamuel@inforgeneses.com.br>
2020-11-03 19:15:43 -0300

    adicionado mascaras de data e hora; adicionado exemplos

--- mask_helper.js
+++ mask_helper.js
@@ -24,7 +24,7 @@ const mask_helper = (_ => {
 		.replace(/\D/g, '')
 		.replace(/(\d{2})(\d)/, '($1) $2')
 		.replace(/(\d{4})(\d)/, '$1-$2')
-		.replace(/(\d{4})-(\d)(\d{4})/, '$1-$2') // tratando numeros com 4 digitos antes do -
+		.replace(/(\d{4})-(\d)(\d{4})/, '$1$2-$3') // tratando numeros com 4 digitos antes do -
 		.replace(/(-\d{4})\d+?$/, '$1')
 
 	// 99999-999
@@ -32,6 +32,23 @@ const mask_helper = (_ => {
 		.replace(/\D/g, '')
 		.replace(/(\d{5})(\d)/, '$1-$2')
 		.replace(/(-\d{3})\d+?$/, '$1')
+
+	// 01/01/1001
+	vm.date_ddmmyyyy = value => value
+		.replace(/\D/g, '')
+		.replace(/(\d{2})(\d)/, '$1/$2')
+		.replace(/(\d{2})(\d)/, '$1/$2')
+		.replace(/(\/\d{4})\d+?$/, '$1')
+
+	// 01/01/1001 12:15:00 todo fix me
+	vm.date_ddmmyyyy_hhiiss = value => value
+		.replace(/\D/g, '')
+		.replace(/(\d{2})(\d)/, '$1/$2') // quando existirem 2 numeros e surgir um terceiro numero
+		.replace(/(\d{2})(\d)/, '$1/$2')
+		.replace(/(\d{4})(\d)/, '$1 $2')
+		.replace(/( \d{2})(\d)/, '$1:$2')
+		.replace(/(\:\d{2})(\d)/, '$1:$2')
+		.replace(/(\:\d{2}\:\d{2})\d+?$/, '$1')
 
 	// 999.99999.99-9
 	vm.pis = value => value
