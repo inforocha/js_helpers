0423dce8269a40aba8d1f4b85b3414a83a1495c8
Davi Samuel de O. Rocha <info.rocha2@gmail.com>
2022-12-27 19:15:05 -0300

    Adicionanando função de agrupamento de arrays

--- array_helper.js
+++ array_helper.js
@@ -1,5 +1,23 @@
+/**
+ * @version 1.1
+ */
 const array_helper = (_ => {
 	const vm = {}
+
+	/**
+	 * [en] Group multiple arrays by a key.
+	 * [pt-BR] Agrupa varios arrays por uma chave.
+	 * @param array arr
+	 * @param string key
+	 * @return array
+	 *
+	 * @example
+	 *	console.log(array_helper.groupBy(['one', 'two', 'three'], 'length')); // {"3": ["one", "two"], "5": ["three"]
+	 */
+	vm.groupBy = (data, key) => data.reduce((rv, x) => {
+		(rv[x[key]] = rv[x[key]] || []).push(x);
+		return rv;
+	}, {})
 
 	/**
 	* [en] Returns an array without duplicate values ​​from the original array.
@@ -24,21 +42,11 @@ const array_helper = (_ => {
 	* 	console.log(array_helper.arrayCompare([1,2,1,3,2],[1,2,1,3,2])) // true
 	* 	console.log(array_helper.arrayCompare([1,2,1,3,2],[1,'A',1,3,2])) // false
 	*/
-	vm.arrayCompare = (a1, a2) => {
-		return JSON.stringify(a1) == JSON.stringify(a2)
-		// const length = a2.length;
+	vm.arrayCompare = (a1, a2) => JSON.stringify(a1) == JSON.stringify(a2)
 
-		// if (a1.length != length) return false;
-
-		// for (let i = 0; i < length; i++) {
-		// 	if (a1[i] !== a2[i]) return false;
-		// }
-
-		// return true;
-	}
 
 	/**
-	* @deprecated utilizar a funcao de array some
+	* @deprecated pode utilizar a funcao de array some similar ao primeiro exemplo.
 	* [en] Checks whether an item exists in an array. Unlike includes, this function compares similarity of objects.
 	* [pt-BR] Verifica se existe um item em um array. Diferente do includes, esta funcao compara similaridade de objetos.
 	* @param mixed needle - o que deseja saber
@@ -57,20 +65,7 @@ const array_helper = (_ => {
 	* 	['joe', 'jane', 'mary',['jane']].includes(['jane']) // false
 	* 	['joe', 'jane', 'mary',['jane']].some(obj => obj == ['jane']) // true
 	*/
-	vm.inArray = (needle, haystack) => {
-		return haystack.some(obj => vm.arrayCompare(obj,needle))
-		// const length = haystack.length;
-
-		// for(let i = 0; i < length; i++) {
-		// 	if(typeof haystack[i] == 'object') {
-		// 		if(vm.arrayCompare(haystack[i], needle)) return true;
-		// 	} else {
-		// 		if(haystack[i] == needle) return true;
-		// 	}
-		// }
-
-		// return false;
-	}
+	vm.inArray = (needle, haystack) => haystack.some(obj => vm.arrayCompare(obj,needle))
 
 	return vm
 })();
